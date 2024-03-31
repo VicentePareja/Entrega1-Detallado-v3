19a77c95b3d414b6db4475026d455095a487a241
Vicente Pareja <vicente.pareja@uc.cl>
2024-03-31 18:51:56 -0300

    Setup inicial

---
+++ Fire-Emblem/Fire-Emblem/Fire-Emblem/Game.cs
@@ -0,0 +1,21 @@
+﻿using Fire_Emblem_View;
+
+namespace Fire_Emblem;
+
+public class Game
+{
+    private View _view;
+    private string _teamsFolder;
+    
+    public Game(View view, string teamsFolder)
+    {
+        _view = view;
+        _teamsFolder = teamsFolder;
+    }
+
+    public void Play()
+    {
+        _view.WriteLine("Elige un archivo para cargar los equipos");
+        _view.WriteLine("Elige un archivo para cargar los equipos");
+    }
+}
