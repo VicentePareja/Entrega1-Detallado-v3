9c28e062418ffdcc179df1f4133b42d2c1e7acaf
Vicente Pareja <vicente.pareja@uc.cl>
2024-03-31 22:39:56 -0300

    Selección de equipo precaria

---
+++ Fire-Emblem/Fire-Emblem/Fire-Emblem/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Fire_Emblem;
+
+public class Characters
+{
+    [JsonPropertyName("Name")] public string Nombre { get; set; }
+    [JsonPropertyName("Weapon")] public string Arma { get; set; }
+    [JsonPropertyName("Gender")] public string Género { get; set; }
+    [JsonPropertyName("HP")] public int HPmáximo { get; set; }
+    public int HPactual { get; set; }
+    [JsonConverter(typeof(StringToIntConverter))] [JsonPropertyName("Atk")] public int Atk { get; set; }
+    [JsonConverter(typeof(StringToIntConverter))] [JsonPropertyName("Spd")] public int Spd { get; set; }
+    [JsonConverter(typeof(StringToIntConverter))] [JsonPropertyName("Def")] public int Def { get; set; }
+    [JsonConverter(typeof(StringToIntConverter))] [JsonPropertyName("Res")] public int Res { get; set; }
+    public List<Skills> Skills { get; private set; }
+    
+    public Character()
+    {
+        Skills = new List<Skills>();
+    }
+    
+    public void AddSkill(Skills skill)
+    {
+        Skills.Add(skill);
+    }
+}
+
---
+++ Fire-Emblem/Fire-Emblem/Fire-Emblem/Skill.cs
@@ -0,0 +1,27 @@
+﻿namespace Fire_Emblem;
+
+public class Skills
+{
+    
+    {
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    // Constructor
+    public Skills(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    // Método para imprimir los detalles de la habilidad
+    public void ImprimirDetalles()
+    {
+        Console.WriteLine($"Habilidad: {Name}\nDescripción: {Description}\n");
+    }
+}
+}
+
+
+
+    
